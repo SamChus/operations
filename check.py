d0a78d1583d8a6e0f4237c034aa1110f57255a65
Samuel <samuelchigo55@gmail.com>
2026-04-11 02:38:01 +0100

    add print2

--- check.py
+++ check.py
@@ -2,3 +2,8 @@ import sys
 
 print(sys.version)
 print("Hello, World!")
+
+if sys.version_info >= (3, 8):
+    print("You are using Python 3.8 or later.")
+else:
+    print("You are using an older version of Python.")
